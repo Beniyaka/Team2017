c8af8fde0bebb56fc091af74d9529439c8966b27
Iyaka Beni <Iyakabeni@yahoo.com>
2017-02-20 17:02:40 +0400

    test

--- src/main/main.java
+++ src/main/main.java
@@ -2,7 +2,7 @@ package main;
 
 public class main {
 	public static void main(String[] args){
-		System.out.print("Application is loading.....");
+		System.out.print("Application is testing.....");
 	}
 
 }
